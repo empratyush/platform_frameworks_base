f19a4ae4b69793bf85264b9d5ded7690812f555d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-11-15 11:59:29 +0200

    gmscompat: remove unnecessary GmsCompat.maybeEnable()
    newApplication(Class<?> clazz,...) is used only for testing.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1370,7 +1370,6 @@ public class Instrumentation {
     static public Application newApplication(Class<?> clazz, Context context)
             throws InstantiationException, IllegalAccessException, 
             ClassNotFoundException {
-        GmsCompat.maybeEnable(context);
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         return app;
